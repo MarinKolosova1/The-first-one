fa64eec27960b95eba88a120468c78b7099d265e
Marin.Kolosova1 <Hf,jxbq gfhjkm1>
2020-08-30 00:38:01 +0300

    The fifth commit.

--- src/app/App2.java
+++ src/app/App2.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 public class App2 {
     public static void main(String[] args) throws IOException {
-        char c = (char) System.in.read();
-        System.out.println((int)c);
+        int c = System.in.read();
+        System.out.println(c);
     }
 
 }

9b4a32ed47ca8aa4501b4235cc83efc07d2762e5
Marin.Kolosova1 <Hf,jxbq gfhjkm1>
2020-08-30 00:01:24 +0300

    The third commit.

---
+++ src/app/App2.java
@@ -0,0 +1,11 @@
+package app;
+
+import java.io.IOException;
+
+public class App2 {
+    public static void main(String[] args) throws IOException {
+        char c = (char) System.in.read();
+        System.out.println((int)c);
+    }
+
+}
